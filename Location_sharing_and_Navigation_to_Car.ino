446e13a9dd23f009a74b328c28b24949a04a00f6
annamalai0801 <76485331+annamalai0801@users.noreply.github.com>
2020-12-22 20:30:14 +0530

    Add files via upload

---
+++ Location_sharing_and_Navigation_to_Car.ino
@@ -0,0 +1,165 @@
+#include <TinyGPS.h>
+#include <SoftwareSerial.h>
+#include <Keypad.h>
+#include <LiquidCrystal.h>
+SoftwareSerial SIM900(7, 8);
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Constants for row and column sizes
+const int ROW_NUM = 4; //four rows
+const int COLUMN_NUM = 3; //three columns
+
+char keys[ROW_NUM][COLUMN_NUM] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};   //connect to the row pinouts of the keypad
+byte pin_column[COLUMN_NUM] = {5, 4, 3}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
+
+String inputString;
+long inputInt;
+TinyGPS gps;  //Creates a new instance of the TinyGPS object
+const int switchPin = 12;
+
+void setup()
+{
+  Serial.begin(9600);
+  SIM900.begin(9600);  
+  pinMode(switchPin, INPUT);
+  lcd.begin(16, 2);
+  lcd.print("device is active");
+  lcd.clear();
+  inputString.reserve(10); 
+}
+
+void loop()
+{
+  int switchVal;
+  switchVal = digitalRead(switchPin);
+  if (switchVal == HIGH)
+  {
+  {
+    lcd.print("navigate to car");
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (Serial.available())
+    {
+      char c = Serial.read();
+      //Serial.print(c);
+      if (gps.encode(c)) 
+        newData = true;  
+    }
+
+
+  if (newData)      //If newData is true
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);   
+    SIM900.print("AT+CMGF=1\r"); 
+    delay(400);
+    SIM900.println("AT + CMGS = \"+916379611459\"");// recipient's mobile number with country code
+    delay(300);
+    SIM900.print("Latitude = ");
+    SIM900.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    SIM900.print(" Longitude = ");
+    SIM900.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    delay(200);
+    SIM900.println((char)26); // End AT command with a ^Z, ASCII code 26
+    delay(200);
+    SIM900.println();
+    break;
+
+  }
+ 
+  Serial.println(failed);
+ // if (chars == 0)
+   // Serial.println("** No characters received from GPS: check wiring **");
+}
+}
+}
+else
+{
+  
+lcd.print("location sharing");
+  // Get key value if pressed
+  
+ char key = keypad.getKey();
+
+  if (key) 
+  {
+    Serial.println(key);
+
+    if (key >= '0' && key <= '9') 
+    {     // only act on numeric keys
+      inputString += key;               // append new character to input string
+    } 
+    else if (key == '#') 
+    {
+      if (inputString.length() > 0)
+      {
+        inputInt = inputString.toInt(); // YOU GOT AN INTEGER NUMBER
+        inputString = "";               // clear input
+        // DO YOUR WORK HERE
+        lcd.print(inputInt);
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (Serial.available())
+    {
+      char c = Serial.read();
+      //Serial.print(c);
+      if (gps.encode(c)) 
+        newData = true;  
+    }
+
+
+  if (newData)      //If newData is true
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);   
+    SIM900.print("AT+CMGF=1\r"); 
+    delay(400);
+    SIM900.println("AT + CMGS =inputInt");// recipient's mobile number with country code
+    delay(300);
+    SIM900.print("Latitude = ");
+    SIM900.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    SIM900.print(" Longitude = ");
+    SIM900.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    delay(200);
+    SIM900.println((char)26); // End AT command with a ^Z, ASCII code 26
+    delay(200);
+    SIM900.println();
+    
+
+  }
+ 
+  Serial.println(failed);
+ // if (chars == 0)
+   // Serial.println("** No characters received from GPS: check wiring **");
+}
+  
+      }
+    } else if (key == '*') {
+      inputString = "";                 // clear input
+    }
+  }
+} 
+}
+
